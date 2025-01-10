ddec2578f186a3de61ab4ee696f5c58aac5512de
k0pp0ny8 <127405072+BKoppanyD@users.noreply.github.com>
2025-01-10 11:12:40 +0100

    rendrakas
    felesleg torlese

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', () => {
+    const products = document.querySelectorAll('.product-card');
+    products.forEach(product => {
+        const productPosition = product.getBoundingClientRect().top;
+        const screenPosition = window.innerHeight / 1.3;
+        if (productPosition < screenPosition) {
+            product.classList.add('visible');
+        }
+    });
+});
