3df5ac4382991ef861f2389647c17de261910f2b
k0pp0ny8 <127405072+BKoppanyD@users.noreply.github.com>
2025-01-17 09:53:49 +0100

    vegso

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
+function addToCart(productName) {
+    alert(`${productName} a kosárba került!`);
+}
+
+
+
 window.addEventListener('scroll', () => {
+
     const products = document.querySelectorAll('.product-card');
+
     products.forEach(product => {
+
         const productPosition = product.getBoundingClientRect().top;
+
         const screenPosition = window.innerHeight / 1.3;
+
         if (productPosition < screenPosition) {
+
             product.classList.add('visible');
+
         }
+
     });
+
 });
